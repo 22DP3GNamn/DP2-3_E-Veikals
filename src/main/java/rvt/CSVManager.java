5aebc5bb0130e2ea9ba5bf306a194bf102266bc5
Gvido_n <131261294+22DP3GNamn@users.noreply.github.com>
2024-03-05 09:35:15 +0000

    commits

---
+++ src/main/java/rvt/CSVManager.java
@@ -0,0 +1,6 @@
+package rvt;
+
+public class CSVManager {
+    //Create a method that will read the CSV file and iy will save inputs in PersonTable.csv?
+    
+}
--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -1,17 +1,26 @@
 package rvt;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.HashMap;
 
+// import javax.naming.Binding;
+
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+// import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+// import jakarta.servlet.http.HttpServletRequest;
+// import javax.validation.Valid;
 
 @Controller
 public class DefaultController {
     
+<<<<<<< HEAD
     @GetMapping(value = "/")
     ModelAndView index(@RequestParam(name="name", required=false, defaultValue="null") String name) {
         Person person = new Person("John", "Smith", "a@a.lv", "DP2-4");
@@ -21,6 +30,58 @@ public class DefaultController {
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("persona", persona);
         modelAndView.addObject("date", new Date().toString());
+=======
+   @GetMapping(value = "/")
+    String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
+        return "index";
+    }
+
+    @GetMapping(value = "/hello")
+    String hello(){
+        return "hello";
+    } 
+
+    @GetMapping(value = "/about")
+    ModelAndView about(){
+        ModelAndView modelAndView = new ModelAndView("about");
+    return modelAndView;
+    }
+
+    @GetMapping(value = "/Links")
+    String Links(){
+        return "Links";
+    }
+
+    @GetMapping(value = "/Experiment")
+    String Experiment(){
+        return "Experiment";
+    }
+
+    @GetMapping(value = "/test")
+    public ModelAndView testAction(){
+        ModelAndView modelAndView = new ModelAndView("test");
+>>>>>>> 2f757cb (commits)
         return modelAndView;
     }
+
+    @GetMapping(value = "/registration")
+    public ModelAndView register(@RequestParam HashMap<String, String> allParams, User user){
+        if(allParams.containsKey("success")){
+            ModelAndView modelAndView = new ModelAndView("success");
+            return modelAndView;
+        }
+        ModelAndView modelAndView = new ModelAndView("registration");
+        return modelAndView;
+    }
+
+    // @PostMapping(value = "/registration")
+    // public String register(@Valid @ModelAttribute("student")Person person, BindingResult bindingResult){
+    //     if(bindingResult.hasErrors()){
+    //         return "/registration";
+    //     }
+    //     else{
+    //         System.out.println(person);
+    //         return "redirect:/registration?success";
+    //     }
+    // }
 }
